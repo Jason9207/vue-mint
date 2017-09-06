699b7501a8ca042152d4915d263fd738cf9a5298
Jason <569038973@qq.com>
2017-09-06 18:01:54 +0800

    mint

---
+++ src/pages/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <mt-tabbar>
+    <mt-tab-item id="外卖">
+      <img slot="icon" src="../../static/images/feedback.png">
+      外卖
+    </mt-tab-item>
+    <mt-tab-item id="订单">
+      <img slot="icon" src="../../static/images/feedback.png">
+      订单
+    </mt-tab-item>
+    <mt-tab-item id="发现">
+      <img slot="icon" src="../../static/images/feedback.png">
+      发现
+    </mt-tab-item>
+    <mt-tab-item id="我的">
+      <img slot="icon" src="../../static/images/feedback.png">
+      我的
+    </mt-tab-item>
+  </mt-tabbar>
+</template>
+
+<script>
+import Vue from 'vue'
+import { Tabbar, TabItem } from 'mint-ui'
+Vue.component(Tabbar.name, Tabbar)
+Vue.component(TabItem.name, TabItem)
+</script>
